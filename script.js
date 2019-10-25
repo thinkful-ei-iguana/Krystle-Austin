6f5a2a484019911fce5c8e7ccfa7f6b5cf4bca50
Austin Tumlinson <54049192+AustinT33@users.noreply.github.com>
2019-10-25 14:20:31 -0500

    Quiz App
    Quiz App files and outline

---
+++ script.js
@@ -0,0 +1,47 @@
+// when a user clicks on start quiz button 
+
+
+
+
+//Displays question number and score obtained
+
+
+
+
+
+
+//Displays the options for the current question
+
+
+
+
+
+
+//displays the question
+
+
+
+
+
+
+
+//displays results and restart quiz button
+
+
+
+
+
+
+//checks whether it reached the end of questions list
+
+
+
+
+
+
+
+//checks whether the chosen option is right or wrong and displays respective message
+
+
+
+
---
+++ store.js
@@ -0,0 +1,60 @@
+const store = {
+  questions: [
+    {
+      question: "Why does the Wizard wear a hood?",
+      options: [
+        "To focus channel his fireball",
+        "To cloak himself during battle",
+        "To hide his hair-do"
+        "To keep his hair clean"
+      ],
+      answer: "To hide his hair-do"
+    },
+
+    {
+      question: "What does P.E.K.K.A stand for?",
+      options: [
+        "Pure, Energy, Knight, Killer, Armour",
+        "Nobody knows", 
+        "Placing, Epic, King, Killer, Armour",
+        "Something gibberish"
+      ],
+      answer: "Nobody Knows"
+    },
+
+    {
+      question: "What is the first air target only building unlocked",
+      options: [
+        "Archer Tower",
+        "Inferno Tower", 
+        "Hidden-Tesla",
+        "Air Defense"
+      ],
+      answer: "Air Defense"
+    },
+
+    {
+      question: "Who does Hog Rider look like?",
+      options: [
+        "His hog",
+        "Thor", 
+        "Mr-T",
+        "His own special person"
+      ],
+      answer: "Mr-T"
+    },
+
+    {
+      question: "What kind of troop is the Barbarian?",
+      options: [
+        "Gentle and loves cats",
+        "Crazy and Enraged", 
+        "Stealthy and decisive",
+        "Agile and defensive"
+      ],
+      answer: "Crazy and Enraged"
+    }
+  ],
+  currentQuestion: 0,
+  score: 0
+};
